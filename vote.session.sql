9e45d7f029ec35058e0c26388b11250f94410c86
b9891796435 <76828507+b9891796435@users.noreply.github.com>
2023-09-29 11:06:05 +0800

    Delete vote.session.sql

--- vote.session.sql
+++
@@ -1,8 +0,0 @@
--- SELECT vote.cid,
---     cname,
---     count(*) AS votes
--- from vote
---     LEFT JOIN candidate ON vote.cid = candidate.cid
--- GROUP BY cid;
--- SELECT * FROM user WHERE username = 'diana' AND password = 'hopefuldreamer';
-INSERT INTO vote (cid,uid) VALUES (3,10)
